a6043bcb4de1c8448d7b975c0e09c0ca1c802d7f
lmcginnisno1 <lmcginnisno1@gmail.com>
2025-01-07 20:57:49 -0500

    fixed elevator autolog

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorIO.java
@@ -5,10 +5,13 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ElevatorIO {
   @AutoLog
   public static class ElevatorIoInputs {
-    public double m_liftLeftPos;
-    public double m_liftRightPos;
+    public double m_liftPos;
     public double m_liftGoal;
   }
+
+  public default void updateInputs(ElevatorIoInputs inputs) {}
+
+  public default void setGoal(){}
 
   public default void updateInputs(ElevatorIoInputs inputs) {}
 
