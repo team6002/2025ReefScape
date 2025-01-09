7fa7a7aceb8ad2069125b15445aa4b22ef5bfc25
lmcginnisno1 <lmcginnisno1@gmail.com>
2025-01-09 18:55:47 -0500

    integrated vision code

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorIO.java
@@ -5,7 +5,8 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ElevatorIO {
   @AutoLog
   public static class ElevatorIoInputs {
-    public double m_liftPos;
+    public double m_liftLeftPos;
+    public double m_liftRightPos;
     public double m_liftGoal;
   }
 
